86f666e4f0853e351d2cd065794e7e01a76d48a4
din2011 <erdingan@gmail.com>
2013-10-25 08:28:56 +0800

    first commit

---
+++ search/data_imevidence.js
@@ -0,0 +1,64 @@
+var abstracts = new Array();
+var authors = new Array();
+//var pdfs = new Array();
+//var citations = new Array();
+
+var datePublications = new Array();
+var keywords = new Array();
+
+var idReview = new Array();
+//
+idReview['Tea tree (Melaleuca alternifolia) oil as treatment for acne vulgaris'] = 1;
+idReview['Music therapy for insomnia'] = 2;
+idReview['Homeopathy for treatment of headache disorders'] = 3;
+idReview['Essential oil as an antiobesity agent'] = 4;
+
+var idReviewRev = new Array();
+idReviewRev[1] = 'Tea tree (Melaleuca alternifolia) oil as treatment for acne vulgaris';
+idReviewRev[2] = 'Music therapy for insomnia';
+idReviewRev[3] = 'Homeopathy for treatment of headache disorders';
+idReviewRev[4] = 'Essential oil as an antiobesity agent';
+
+
+abstracts[1] = "abstracts for tea tree";
+authors[1] = "Mr Tea Tree & friends";
+//pdfs[1] = "";
+//citations[1] = "";
+datePublications[1] = "1 Jan 2014";
+keywords[1] = "tea tree, acne";
+
+abstracts[2] = 
+        "This paper is a report of a meta-analysis conducted to evaluate the efficacy of music-assisted relaxation for sleep quality in adults and elders with sleep complaints with or without a co-morbid medical condition."+
+"Background. Clinical studies have shown that music can influence treatment outcome in a positive and beneficial way. Music holds the promise of counteracting psychological presleep arousal and thus improving the preconditions for sleep.";
+        
+authors[2] = "Dr Ami, etc";
+//pdfs[2] = "";
+//citations[2] = "";
+datePublications[2] = "1 December 2013";
+keywords[2] = "music therapy, insomnia";
+
+abstracts[3] = "abstract for Homeopathy for treatment of headache disorders";
+authors[3] = "Terence Tan, etc";
+//pdfs[3] = "";
+//citations[3] = "";
+datePublications[3] = "2 December 2013";
+keywords[3] = "Homeopathy, headache disorders";
+
+abstracts[4] = "abstract for Essential oil as an antiobesity agent";
+authors[4] = "Dr Aswir etc";
+//pdfs[4] = "";
+//citations[4] = "";
+datePublications[4] = "1 Feb 2014";
+keywords[4] = "Essential oil, antiobesity agent";
+
+/*
+ * 
+ * 
+ * 
+ abstracts[''] = "";
+authors[''] = "";
+pdfs[''] = "";
+citations[''] = "";
+datePublications[''] = "";
+keywords[''] = "";
+ */
---
+++ search/index_files/rokutils.js
@@ -0,0 +1,8 @@
+/**
+ * @version		1.2 July 9, 2012
+ * @author		RocketTheme, LLC http://www.rockettheme.com
+ * @copyright 	Copyright (C) 2007 - 2012 RocketTheme, LLC
+ * @license		http://www.rockettheme.com/legal/license.php RocketTheme Proprietary Use License
+ */
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5.7(\'J\',4(){3 b=k.l(\'E-D\');1(b){3 c=A z.y(5);b.6(\'v\',\'r\').7(\'t\',4(e){e.u();c.s()})};1(q.p.9){3 d=$$(\'.n-C\'),2=$$(\'.n-G\');1(d.8&&2.8){d=d[0];2=2[0];3 f=2.h().x;d.6(\'o\',f);5.7(\'K\',4(){3 a=2.h().x;d.6(\'o\',a)})}}1(q.p.9){3 g=k.l(\'F-B\').w(\'.m\');1(g.8){g.H(4(a,i){1(a.I(\'j\'))a.L(\'m-j\')})}}});',48,48,'|if|showcasesurround|var|function|window|setStyle|addEvent|length|trident4||||||||getSize||daddy|document|id|separator|showcase|width|Engine|Browser|none|toTop|click|stop|outline|getElements||Scroll|Fx|new|menu|bar|scroll|top|horiz|surround|each|hasClass|domready|resize|addClass'.split('|'),0,{}))
